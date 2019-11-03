2912e14f74fb9daba1a9cff2953e05816af54425
ngqiming <44829605+ngqiming@users.noreply.github.com>
2019-11-03 14:36:41 +0800

    Updated Part 1 to be correctly plotting graph

--- LabProject3006.m
+++ LabProject3006.m
@@ -1,34 +1,33 @@
-%Seed creation
-% rng();
-% s=rng;
-% 
-% rng(s);
-
 %Generate 0 and 1 bit where N = 1024
 NumOfBits = 1024;
 input = randi([0, 1], [1, 1024]);
 tempInput = input;
-%Convert binary digits to +1 and -! 
+%Convert binary digits to +1 and -1
 input(input==0) = [-1];
 bitErrorRateOutput = zeros(1,11)
 counter=1;
 SNRAxis = zeros(1,11);
+
+
+
 for i = 0:5:50
     SNRAxis(counter) = i;
-    bitErrorRate = calculate_error_rate(input, tempInput, i)
+    bitErrorRate = calculate_error_rate(input, tempInput, i);
     bitErrorRateOutput(counter)=bitErrorRate;
     counter = counter +1;
 end
-plot(SNRAxis, bitErrorRateOutput);
+semilogy(SNRAxis, bitErrorRateOutput);
+axis([0 50 -1 1])
+
 function bitErrorRate = calculate_error_rate(input, tempInput, SNR)
     %Generate noise having normal distribution with zero mean 
     b = 0; %mean
-    NumOfBits = 1023;
-
-    NoiseVariance = 1/ power(10,SNR/10);
+    NumOfBits = 1024;
+    threshold = 0;
+    NoiseVariance = 1/ 10^(SNR/10);
     NoiseSTD = sqrt(NoiseVariance); %SD
     noise = NoiseSTD.*randn(1,1024) + b;
-    stats = [mean(noise) std(noise) var(noise)];
+    %stats = [mean(noise) std(noise) var(noise)];
 
     %Add noise sample with input data
     output = input + noise; 
@@ -39,8 +38,14 @@ function bitErrorRate = calculate_error_rate(input, tempInput, SNR)
     output(output<0) = [0];
 
     %Compute p(e) 
-    bitError = biterr(tempInput,output);
+    bitError = 0
+    for i=1 : length(output)
+        if (tempInput(i) > threshold && output(i) == 0) || (tempInput(i) <= threshold && output(i) == 1)
+            bitError = bitError + 1;
+        end
+    %bitError = biterr(tempInput,output);
     bitErrorRate= bitError/NumOfBits;
+    end
 end
 
 
